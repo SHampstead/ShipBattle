81a3f587c12d8ca6e66e56da1c6399a044184391
SHampstead <simonhampstead@hotmail.com>
2023-08-30 17:21:01 +0100

    Continuing class and game input work. Added weapon and support module classes and introduction text.

--- Classes_SB.py
+++ Classes_SB.py
@@ -13,3 +13,26 @@ class Ship:
         print("""
         {name} is a {make} class with a large {large}, and medium {medium}, and a samll {small}. It is supported by a {support} module and has {health} HP and {shield} SP.
         """.format(name=self.name, make=self.make, large=self.large, medium=self.medium, small=self.small, support=self.support, health=self.health, shield=self.shield))
+
+class Weapon:
+    def __init__(self, name, damage, effect):
+        self.name = name
+        self.damage = damage
+        self.effect = effect
+
+    def __repr__(self):
+        print("""
+        {name} does {damage} points of {effect} damage
+        """.format(name=self.name, damage=self.damage, effect=self.effect))
+
+class Support:
+    def __init__(self, name, effect, system):
+        self.name = name
+        self.effect = effect
+        self.system = system
+
+    def __repr__(self):
+        print("""
+        {name} is a support module that increases {system} points by {effect}
+        """)
+        
--- ShipBattle.py
+++ ShipBattle.py
@@ -1,4 +1,15 @@
 from Classes_SB import *
-
+from sys import exit
 
 print("hello and welcome to Ship Battle.")
+players = input("Are there two palyers? ")
+if "y" in players.lower():
+    print("Great! lets begin!")
+else:
+    print("Please get another player!")
+    exit()
+
+player1_name = input("Player 1, what is your name? ")
+player2_name = input("Player 2, what is your name? ")
+
+
